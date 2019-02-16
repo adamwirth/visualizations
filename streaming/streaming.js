42776f7d5f301c4cf741e16ba6f8b9357b3960f6
Adam Wirth <adamowirth@gmail.com>
2019-02-16 15:27:39 -0500

    getting somewhere!
    it's logging Promise objects as strings instead of rendering bars...
    but adding async + await is helping
    progress

--- streaming/streaming.js
+++ streaming/streaming.js
@@ -4,13 +4,14 @@ import { require } from 'https://unpkg.com/d3-require@1?module';
 function nap () { return setTimeout(function() {}, Math.random() * 3) && true }
 
 /** Store a time then return the datum */
+// TODO function* ? other ways to approach
 const pointMaker = async () => {
-    const x = new Date().getTime()
-    nap()
+    const x = new Date().getTime();
+    nap();
     return {
         data: {
             x,
-            y: Math.random() * 100
+            y: Math.random() * 100,
         }
     }
 }
@@ -21,7 +22,7 @@ export default async function chart(selector='body') {
 
     const margin = { top: 10, right: 30, bottom: 30, left: 30 },
         width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom,
+        height = 440 - margin.top - margin.bottom,
         barWidth = Math.floor(width / 10);
     
     // X
@@ -46,29 +47,36 @@ export default async function chart(selector='body') {
         .attr('class', 'axis axis--x')
         .attr('transform', `translate(0,${height})`)
         .call(d3.axisBottom(x));
+
+    const points = []
+    for (let i = 0; i < 5; i += 1) {
+        points.push(pointMaker());
+    }
     
     // Bars
     const bar = svg.selectAll('.bar')
-        .data([])
-            .enter().append('g')
+        .data(points, async function(key, i, j) { 
+            console.log(key)
+            const thing = await key;
+            return thing.y;
+        })
+        .enter(points, async function(d) {
+            console.log('enter', d);
+            return await d;
+         })
+        .append('g')
         .attr('class', 'bar')
         .attr('transform', function (d, i) { return `translate(${i * barWidth},0)`; });
 
     bar.append('rect')
-        .attr('y', function(d) { return y(d.y); })
+        .attr('y', async function(d) { return y(await d.y); })
         .attr('width', barWidth - 1)
-        .attr('height', function (d) { return height - y(d.y); });
+        .attr('height', async function (d) { return height - y(await d.y); });
 
     bar.append('text')
         .attr('x', barWidth / 2)
-        .attr('dy', '.75rem')
+        .attr('dy', '.75')
         .attr('y', 8)
-        .text(function (d) { return d.y; });
+        .text(async function (d) { return await d.y; });
 
-    for (let i = 0; i < 3; i += 1) {
-        const point = pointMaker()
-        console.log(point);
-        bar.insert(point);
-        nap();
-    }
 }
