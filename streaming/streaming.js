986646788cb9b3c0caf6646b2ffa5ec815f3adc6
Adam Wirth <adamowirth@gmail.com>
2019-03-09 10:18:16 -0500

    refactoring
    removing async content and code splitting for a moment. just getting the rects to render!

--- streaming/streaming.js
+++ streaming/streaming.js
@@ -1,5 +1,3 @@
-import { require } from 'https://unpkg.com/d3-require@1?module';
-
 function nap () { setTimeout(function() {}, Math.random() * 3); }
 
 /** Store a time then return the datum */
@@ -15,70 +13,11 @@ export const pointMaker = async () => {
     }
 }
 
-export default async function chart(selector='body') {
-
-    const d3 = await require('d3@5');
-
+export default function chartArgs() {
     const margin = { top: 10, right: 30, bottom: 30, left: 30 },
         width = 960 - margin.left - margin.right,
         height = 440 - margin.top - margin.bottom,
         barWidth = Math.floor(width / 10);
-    
-    // X
-    const x = d3.scaleTime()
-        .domain([Date.now() - 100000, Date.now() + 1000000])
-        .rangeRound([0, width]);
 
-    // Y
-    const y = d3.scaleLinear([height, 0], [0, 100]);
-
-    // Chart
-    const svg = d3.select(selector).append('svg')
-        .attr('width', width + margin.left + margin.right)
-        .attr('height', height + margin.top + margin.bottom)
-        .append('g')
-        .attr('transform', `translate(${margin.left},${margin.top})`);
-
-    // Axis
-    svg.append('g')
-        .attr('class', 'axis axis--x')
-        .attr('transform', `translate(0,${height})`)
-        .call(d3.axisBottom(x));
-    
-    // Bars
-    const bar = svg.selectAll('.bar')
-        .data([], async function(key, i, j) { 
-            const thing = await key;
-            console.log(thing);
-            return thing;
-        })
-        .enter([], async function(d) {
-            console.log('enter', d);
-            return await d;
-         });
-
-    bar.append('rect')
-        .attr('y', async function(d) { 
-            const val = await d;
-            console.log('y', val);
-            return y(val.y);
-        })
-        .attr('width', barWidth - 1)
-        .attr('height', async function (d) {
-            const val = await d;
-            console.log('height', val)
-            return height - y(val.y); 
-        });
-
-    bar.append('text')
-        .attr('x', barWidth / 2)
-        .attr('dy', '.75')
-        .attr('y', 8)
-        .text(async function (d) {
-            const val = await d;
-            console.log('text', val)
-            return val.y; 
-        });
-
-    return { bar, x, y, height, width, barWidth };
+    return { height, width, margin, barWidth };
 }
