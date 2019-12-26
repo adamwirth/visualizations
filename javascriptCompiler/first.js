bc035cc93adbf5792183a6c80af9a63c0af0fd73
Adam Wirth <adamowirth@gmail.com>
2019-12-26 10:20:45 -0500

    trying sequencing. spent a while figuring out I shouldve passed an array smh

--- javascriptCompiler/first.js
+++ javascriptCompiler/first.js
@@ -8,21 +8,40 @@
 // CMD node ./javascriptCompiler/first.js
 const A = require('arcsecond')
 
-const helloWorld = 'hello world'
-// only likes this string, however many times now, and now removes case. doesnt seem to ignore case
-// TODO curious how this could be refactored to extract functions?
-const stringParser = A.many(A.str(helloWorld))
-    .map(results =>
-        results.map(s => s.toUpperCase()).join(''))
+// ok now it should create a subsection for each of these. and the .many should nest subsections of whitespace (just the space character)
+const stringParser = A.sequenceOf([
+    A.letters,
+    A.digits,
+    A.str('hello'),
+    A.many(A.char(' ')),
+    A.str('world'),
+    A.endOfInput,
+])
 
-
-console.log(stringParser.run(
-    helloWorld + helloWorld + 'hello WORLD'
-))
-// TODO misunderstood - the results is being mapped -- not the parsing part
+console.log(
+    stringParser.run(
+        'asdf1234hello   world'
+    )
+)
 /*
 { isError: false,
-  result: 'HELLO WORLDHELLO WORLD',
-  index: 22,
+  result:
+   [ 'asdf', '1234', 'hello', [ ' ', ' ', ' ' ], 'world', null ],
+  index: 21,
   data: null }
-  */
+*/
+
+// cares about order, this wont work
+console.log(
+    stringParser.run(
+        'asdf1234     helloworld '
+    )
+)
+
+/*
+{ isError: true,
+error:
+"ParseError (position 8): Expecting string 'hello', got '     ...'",
+index: 8,
+data: null }
+*/
