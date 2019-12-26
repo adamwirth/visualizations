ca8307be6888890b996e48308b689ce657639299
Adam Wirth <adamowirth@gmail.com>
2019-12-26 10:39:06 -0500

    starting in on a new tutorial and file

---
+++ javascriptCompiler/second.js
@@ -0,0 +1,25 @@
+const A = require('arcsecond')
+
+// ref tutorial https://youtu.be/1axJDmK_pnE?list=PLP29wDx6QmW5yfO1LAgO8kU3aQEj8SIrU
+
+// "curried" function.
+// todo why is it called currying anyway...
+const str = s => targetString => {
+    if (targetString.startsWith(s)) {
+        return s
+    }
+
+    throw new Error(`Tried to match ${s}, instead got "${targetString.slice(0, 10)}..."`)
+}
+
+const run = (parser, targetString) => {
+    return parser(targetString)
+}
+
+const parser = str('hello world!')
+
+run(parser, 'This will not work.')
+// throws the error
+
+run(parser, 'hello world! This should work')
+// doesn't print anything / works
