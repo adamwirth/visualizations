fbfd2d6de9a0b0f29d5ae5fc84cd030d06ba36cf
Adam Wirth <adamowirth@gmail.com>
2019-12-26 11:14:55 -0500

    method refactoring, utility function extractions

--- javascriptCompiler/second.js
+++ javascriptCompiler/second.js
@@ -3,6 +3,26 @@
 const trailMax = 10 // its like trailmix haha
 const trail = s => s.length > trailMax ? `"${s.slice(0, trailMax)}..."` : `"${s}"`
 
+// utility function. reminder we are aiming for immutability in our system; this should return a NEW object.
+const updateParserState = (state, index, result) => ({
+    ...state,
+    index,
+    result,
+})
+
+// Don't update index
+const updateParserResult = (state, result) => ({
+    ...state,
+    result,
+})
+
+// utility function. add errormsg, now isError
+const updateParserError = (state, errorMsg) => ({
+    ...state,
+    isError: true,
+    error: errorMsg,
+})
+
 // "curried" function.
 // todo why is it called currying anyway...
 
@@ -13,25 +33,31 @@ const str = s => parserState => {
     const {
         targetString,
         index,
+        isError,
      } = parserState
 
-    if (targetString.slice(index).startsWith(s)) {
-        return {
-            ...parserState,
-            // NOTE result + index override the parserState's spread
-            result: s,
-            index: index + s.length,
-        }
+    // return parser error all the way back down/(up?) to the user. this will allow us to catch on to unexpected errors
+    if (isError) {
+        return parserState
     }
 
-    return {
-        ...parserState,
-        error: `Tried to match "${s}", instead got ${trail(targetString)}`,
-        isError: true,
+    if (targetString.slice(index).startsWith(s)) {
+        return updateParserState(
+            parserState, index + s.length, s,
+        )
     }
+
+    return updateParserError(
+        parserState,
+        `Tried to match "${s}", instead got ${trail(targetString)}`,
+    )
 }
 
 const sequenceOf = parsers => parserState => {
+    if (parserState.isError) {
+        return parserState
+    }
+
     const results = []
     let nextState = parserState
 
@@ -40,11 +66,10 @@ const sequenceOf = parsers => parserState => {
         results.push(nextState.result)
     }
 
-    return {
-        ...nextState,
-        // overrides the spread's result of with the array of result
-        result: results,
-    }
+    return updateParserResult(
+        nextState,
+        results,
+    )
 }
 
 // parsers should all be receiving and returning a "Parser State" object
@@ -53,6 +78,8 @@ const run = (parser, targetString) => {
         targetString,
         index: 0,
         result: null,
+        isError: false,
+        error: null,
     }
 
     // need to pass a state object instead now
@@ -62,12 +89,12 @@ const run = (parser, targetString) => {
 const parser = str('hello world')
 
 console.log(
-    run(parser, 'gyoza')
+    run(parser, 'hello world!')
 )
 /*
-{ targetString: 'gyoza',
-  index: 0,
-  result: null,
-  error: 'Tried to match "hello world", instead got "gyoza"',
-  isError: true }
+{ targetString: 'hello world!',
+  index: 11,
+  result: 'hello world',
+  isError: false,
+  error: null }
 */
